massbitprotocol/ethereum-indexer

all: Change how WritableStore tracks the block pointer and cursor

chain/ethereum/src/chain.rs
@@ -147,7 +147,7 @@ impl Chain {
             true => 0,
         };
 
-        let start_block = writable.block_ptr()?;
+        let start_block = writable.block_ptr();
 
         Ok(Box::new(PollingBlockStream::new(
             writable,
@@ -187,7 +187,7 @@ impl Chain {
             .new(o!("component" => "FirehoseBlockStream"));
 
         let firehose_mapper = Arc::new(FirehoseMapper {});
-        let firehose_cursor = store.block_cursor()?;
+        let firehose_cursor = store.block_cursor();
 
         Ok(Box::new(FirehoseBlockStream::new(
             firehose_endpoint,

BladeCKG/local-graph-node

chain/ethereum/src/chain.rs
@@ -217,7 +217,7 @@ impl Blockchain for Chain {
             .new(o!("component" => "FirehoseBlockStream"));
 
         let firehose_mapper = Arc::new(FirehoseMapper {});
-        let firehose_cursor = writable.block_cursor()?;
+        let firehose_cursor = writable.block_cursor();
 
         Ok(Box::new(FirehoseBlockStream::new(
             firehose_endpoint,